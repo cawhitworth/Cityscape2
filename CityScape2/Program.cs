40f1a731fb3ed8d7987e1a154ac0ab9ea7b74a9c
Chris Whitworth <chris.whitworth@red-gate.com>
2014-12-10 10:31:35 +0000

    Initial commit.

---
+++ CityScape2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CityScape2
+{
+    internal static class Program
+    {
+
+        [STAThread]
+        static void Main()
+        {
+            
+        }
+    }
+}
