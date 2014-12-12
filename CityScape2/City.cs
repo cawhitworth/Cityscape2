bab5bd105b44aaeac9a4f387fbd713bee5ddefd3
Chris Whitworth <chris.whitworth@red-gate.com>
2014-12-12 10:30:29 +0000

    Extract vertex rendering logic.

--- CityScape2/City.cs
+++ CityScape2/City.cs
@@ -2,10 +2,7 @@
 using System.IO;
 using SharpDX;
 using SharpDX.Direct3D11;
-using SharpDX.DXGI;
-
 using Device = SharpDX.Direct3D11.Device;
-using MapFlags = SharpDX.Direct3D11.MapFlags;
 
 namespace CityScape2
 {
@@ -13,12 +10,11 @@ namespace CityScape2
     {
         private readonly Device m_Device;
         private readonly DeviceContext m_Context;
-        private VertexShader m_VertexShader;
-        private InputLayout m_Layout;
         private PixelShader m_PixelShader;
-        private Buffer m_ConstantBuffer;
         private readonly Texture m_Texture;
         private readonly BatchedGeometryRenderer m_BatchedRenderer;
+        private VertexPosNormalTextureShader m_VertexShader;
+
 
         public City(Device device, DeviceContext context)
         {
@@ -27,6 +23,7 @@ namespace CityScape2
 
             LoadShaders();
 
+            m_VertexShader = new VertexPosNormalTextureShader(m_Device);
             m_Texture = Texture.FromFile("texture.png", m_Device);
 
             var boxes = new List<IGeometry>();
@@ -45,28 +42,16 @@ namespace CityScape2
 
             var vertexSize = Utilities.SizeOf<Vector3>()*2 + Utilities.SizeOf<Vector2>();
 
-            m_BatchedRenderer = new BatchedGeometryRenderer(geometryBatcher, m_Device, vertexSize, m_Layout);
+            m_BatchedRenderer = new BatchedGeometryRenderer(geometryBatcher, m_Device, vertexSize, m_VertexShader.Layout);
 
         }
 
         private void LoadShaders()
         {
-            var vertShaderBytecode = File.ReadAllBytes("VertexShader.cso");
-            m_VertexShader = ToDispose(new VertexShader(m_Device, vertShaderBytecode));
-
-            m_Layout = ToDispose(new InputLayout(m_Device, vertShaderBytecode, new[]
-            {
-                new InputElement("POSITION", 0, Format.R32G32B32_Float, 0,0),
-                new InputElement("NORMAL", 0, Format.R32G32B32_Float, 12, 0),
-                new InputElement("TEXCOORD", 0, Format.R32G32_Float, 24, 0), 
-            }));
 
             var pixelShaderBytecode = File.ReadAllBytes("PixelShader.cso");
             m_PixelShader = ToDispose(new PixelShader(m_Device, pixelShaderBytecode));
             
-            m_ConstantBuffer =
-                ToDispose(new Buffer(m_Device, Utilities.SizeOf<Matrix>() * 3, ResourceUsage.Dynamic,
-                    BindFlags.ConstantBuffer, CpuAccessFlags.Write, ResourceOptionFlags.None, 0));
 
         }
 
@@ -76,18 +61,11 @@ namespace CityScape2
             var world = Matrix.RotationY(fElapsed);// * Matrix.RotationX(fElapsed * 0.7f);
             world.Transpose();
 
-            m_Context.VertexShader.Set(m_VertexShader);
-            m_Context.VertexShader.SetConstantBuffer(0, m_ConstantBuffer);
-            m_Context.PixelShader.Set(m_PixelShader);
+            m_VertexShader.Bind(m_Context, world, view, proj);
 
+            m_Context.PixelShader.Set(m_PixelShader);
             m_Texture.Bind(m_Context, 0);
 
-            DataStream mappedResource;
-            m_Context.MapSubresource(m_ConstantBuffer, MapMode.WriteDiscard, MapFlags.None, out mappedResource);
-            mappedResource.Write(world);
-            mappedResource.Write(view);
-            mappedResource.Write(proj);
-            m_Context.UnmapSubresource(m_ConstantBuffer, 0);
 
             return m_BatchedRenderer.Render(m_Context);
 
---
+++ CityScape2/VertexPosNormalTextureShader.cs
@@ -0,0 +1,48 @@
+using System.IO;
+using SharpDX;
+using SharpDX.Direct3D11;
+using SharpDX.DXGI;
+using Device = SharpDX.Direct3D11.Device;
+using MapFlags = SharpDX.Direct3D11.MapFlags;
+
+namespace CityScape2
+{
+    class VertexPosNormalTextureShader : Component
+    {
+        private readonly Buffer m_ConstantBuffer;
+        private readonly VertexShader m_VertexShader;
+        private readonly InputLayout m_Layout;
+
+        public VertexPosNormalTextureShader(Device device)
+        {
+            var vertShaderBytecode = File.ReadAllBytes("VertexShader.cso");
+            m_VertexShader = ToDispose(new VertexShader(device, vertShaderBytecode));
+
+            m_Layout = ToDispose(new InputLayout(device, vertShaderBytecode, new[]
+            {
+                new InputElement("POSITION", 0, Format.R32G32B32_Float, 0,0),
+                new InputElement("NORMAL", 0, Format.R32G32B32_Float, 12, 0),
+                new InputElement("TEXCOORD", 0, Format.R32G32_Float, 24, 0), 
+            }));
+
+            m_ConstantBuffer =
+                ToDispose(new Buffer(device, Utilities.SizeOf<Matrix>() * 3, ResourceUsage.Dynamic,
+                    BindFlags.ConstantBuffer, CpuAccessFlags.Write, ResourceOptionFlags.None, 0));
+        }
+
+        public InputLayout Layout { get { return m_Layout; } }
+
+        public void Bind(DeviceContext context, Matrix world, Matrix view, Matrix proj)
+        {
+            context.VertexShader.Set(m_VertexShader);
+            context.VertexShader.SetConstantBuffer(0, m_ConstantBuffer);
+
+            DataStream mappedResource;
+            context.MapSubresource(m_ConstantBuffer, MapMode.WriteDiscard, MapFlags.None, out mappedResource);
+            mappedResource.Write(world);
+            mappedResource.Write(view);
+            mappedResource.Write(proj);
+            context.UnmapSubresource(m_ConstantBuffer, 0);
+        }
+    }
+}
