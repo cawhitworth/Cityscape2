0f1edaa0804193e6e41d4f678c07019f85d27f98
Chris Whitworth <chris.whitworth@red-gate.com>
2014-12-16 13:21:27 +0000

    Move IInput into its own file

---
+++ CityScape2/IInput.cs
@@ -0,0 +1,12 @@
+﻿using SharpDX;
+using SharpDX.DirectInput;
+
+namespace CityScape2
+{
+    internal interface IInput
+    {
+        bool IsKeyDown(Key key);
+        bool IsKeyUp(Key key);
+        Vector2 MousePosition();
+    }
+}
--- CityScape2/Input.cs
+++ CityScape2/Input.cs
@@ -4,13 +4,6 @@ using SharpDX.DirectInput;
 
 namespace CityScape2
 {
-    internal interface IInput
-    {
-        bool IsKeyDown(Key key);
-        bool IsKeyUp(Key key);
-        Vector2 MousePosition();
-    }
-
     class Input : IInput, IDisposable
     {
         private readonly DirectInput m_Input = new DirectInput();
