a0afca0aca193035dd9c47036a06be6514dcc10b
Chris Whitworth <chris.whitworth@red-gate.com>
2014-12-16 15:34:27 +0000

    Boxes to have no texture.

--- CityScape2/Geometry/Box.cs
+++ CityScape2/Geometry/Box.cs
@@ -11,16 +11,17 @@ namespace CityScape2.Geometry
         public Box(Vector3 c1, Vector3 c2)
         {
             Color mod = Color.White;
-            var front = new Panel(c1, new Vector2(c2.X - c1.X, c2.Y - c1.Y), Panel.Plane.XY, Panel.Facing.Out, new Vector2(0,0), new Vector2(1,1), mod);
-            var back = new Panel(c2, new Vector2(c1.X - c2.X, c1.Y - c2.Y), Panel.Plane.XY, Panel.Facing.In, new Vector2(0,0), new Vector2(1,1), mod);
+            var origin = new Vector2(0.0f);
+            var front = new Panel(c1, new Vector2(c2.X - c1.X, c2.Y - c1.Y), Panel.Plane.XY, Panel.Facing.Out, origin, origin, mod);
+            var back = new Panel(c2, new Vector2(c1.X - c2.X, c1.Y - c2.Y), Panel.Plane.XY, Panel.Facing.In, origin, origin, mod);
             var right = new Panel(new Vector3(c2.X, c1.Y, c1.Z), new Vector2(c2.Z - c1.Z, c2.Y - c1.Y), Panel.Plane.YZ,
-                Panel.Facing.Out, new Vector2(0,0), new Vector2(1,1), mod);
+                Panel.Facing.Out, origin, origin, mod);
             var left = new Panel(new Vector3(c1.X, c2.Y, c2.Z), new Vector2(c1.Z - c2.Z, c1.Y - c2.Y), Panel.Plane.YZ,
-                Panel.Facing.In, new Vector2(0,0), new Vector2(1,1), mod);
+                Panel.Facing.In, origin, origin, mod);
             var top = new Panel(new Vector3(c1.X, c2.Y, c1.Z), new Vector2(c2.X - c1.X, c2.Z - c1.Z), Panel.Plane.XZ,
-                Panel.Facing.Out, new Vector2(0,0), new Vector2(1,1), mod);
+                Panel.Facing.Out, origin, origin, mod);
             var bottom = new Panel(new Vector3(c2.X, c1.Y, c2.Z), new Vector2(c1.X - c2.X, c1.Z - c2.Z), Panel.Plane.XZ,
-                Panel.Facing.In, new Vector2(0,0), new Vector2(1,1), mod);
+                Panel.Facing.In, origin, origin, mod);
 
             m_Aggregate = new AggregateGeometry(front, back, right, left, top, bottom);
         }
